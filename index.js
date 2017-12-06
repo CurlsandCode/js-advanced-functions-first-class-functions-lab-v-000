1eabb9ee1e1122f5c456c1260c1e6168a3f3d921
Monika Williams <monika.m.williams@gmail.com>
2017-12-06 11:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers = function (array) {
 }
 
 const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+function createFareMultiplier (int) {
+  return function (fare) {
+    return fare * int;
+  }
+}
